59eb39a9b3b34b1b6cd60d3555e6d14f54437e9f
Georg Preuss <preussg@gmail.com>
2020-04-14 09:40:57 +0100

    added google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,22 @@
-import { User } from './User'
-import { Company } from './Company'
+// import { User } from './User'
+// import { Company } from './Company'
 
-const user = new User()
-console.log(user)
-const company = new Company()
-console.log(company)
+// this should add a script as the first child
+// const script = document.createElement('script')
+// script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}`
+// document.body.prepend(script)
+
+// this should add a script before the existing script (after map div)
+// const parentElement = document.getElementById('script').parentNode
+// const script = document.getElementById('script')
+// const googleScript = document.createElement('script')
+// googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}`
+// parentElement.insertBefore(googleScript, script)
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 3,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
