6bd48430a675188884adad6253cf93f8aec2de81
Georg Preuss <preussg@gmail.com>
2020-04-16 17:57:29 +0100

    integrated sort method with abstract classes

--- sort_app/src/Sorter.ts
+++ sort_app/src/Sorter.ts
@@ -4,19 +4,21 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {
-  }
+export abstract class Sorter {
+
+  abstract length: number
+  abstract compare(leftIndex: number, rightIndex: number): boolean
+  abstract swap(leftIndex: number, rightIndex: number): void
 
   sort(): void {
-    const { length } = this.collection
+    const { length } = this
 
     // nested for loop to iterate through length - 1 on each iteration
     // e.g. for length 4, iterate through all 4, then first 3, then first 2, then first
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-          if (this.collection.compare(j, j + 1)) {
-            this.collection.swap(j, j + 1)
+          if (this.compare(j, j + 1)) {
+            this.swap(j, j + 1)
           }
       }
     }
