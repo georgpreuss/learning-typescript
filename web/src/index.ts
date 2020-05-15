f6b72655ad6f099589f4f54c07c30fe08345a48a
Georg Preuss <preussg@gmail.com>
2020-05-15 15:25:15 +0100

    extracted eventing

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,5 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'Georg', age: 99})
+const user = new User({ name: 'new record', age: 0 })
 
-user.on('eatBananas', () => {
-  console.log('Change #1')
-})
-user.on('eatBananas', () => {
-  console.log('Change #2')
-})
-user.on('save', () => {
-  console.log('Save was triggered')
-})
-
-user.trigger('eatBananas')
+user.save()
